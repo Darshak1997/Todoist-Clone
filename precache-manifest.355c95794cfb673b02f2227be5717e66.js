08268d329e89e20cc67d11e57750297d3770e5fd
Darshak1997 <42878805+Darshak1997@users.noreply.github.com>
2020-11-06 13:54:56 -0600

    Updates

---
+++ precache-manifest.355c95794cfb673b02f2227be5717e66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2201a9f8aa65a64e86628e50c98f92",
+    "url": "/Todoist-Clone/index.html"
+  },
+  {
+    "revision": "45395ebbbc193e1d1613",
+    "url": "/Todoist-Clone/static/css/main.e59cdcd4.chunk.css"
+  },
+  {
+    "revision": "86aaefa140888710719c",
+    "url": "/Todoist-Clone/static/js/2.24e698b3.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/Todoist-Clone/static/js/2.24e698b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45395ebbbc193e1d1613",
+    "url": "/Todoist-Clone/static/js/main.cf0361aa.chunk.js"
+  },
+  {
+    "revision": "6284739cfb349b30a0c6",
+    "url": "/Todoist-Clone/static/js/runtime-main.c8f27690.js"
+  }
+]);
